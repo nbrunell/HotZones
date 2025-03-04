d07615500b82ac891ce46822d9e005620d0b6c1b
[Maia Kuwabara] <[mkuwabar@asu.edu]>
2025-03-04 07:57:22 -0700

    Added pop out menu, videos page, and changed log out button position

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -72,3 +72,35 @@ async function renderUserList() {
 //////////////////////////////////////////
 //END FUNCTIONS TO MANIPULATE THE DOM
 //////////////////////////////////////////
+
+// Below is js for hamburger menu//
+const hamburger = document.getElementById('hamburger')
+const sidebar = document.getElementById('sidebar')
+const overlay = document.getElementById('overlay')
+
+let menuOpen = false
+
+function openMenu() {
+  menuOpen = true
+  overlay.style.display = 'block'
+  sidebar.style.width = '250px'
+}
+
+function closeMenu() {
+  menuOpen = false
+  overlay.style.display = 'none'
+  sidebar.style.width = '0px'
+}
+
+hamburger.addEventListener('click', function () {
+  if (!menuOpen) {
+    openMenu()
+  }
+})
+
+overlay.addEventListener('click', function () {
+  if (menuOpen) {
+    closeMenu()
+  }
+})
+// END OF MENU//
